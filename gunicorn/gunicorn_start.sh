48ca0ef55364da53e79b4ba7ca731c467df5358c
BenjamenMeyer <ben.meyer@rackspace.com>
2014-10-27 21:43:59 +0000

    Bug Fixes
    - Update gunicorn to have 16 workers and use the correct port
    - Update nginx config for larger data to pass through, 2 MB doesn't cut it for a file transfer application
    - Update config.ini with some basic defaults
    --- sqlite memory cannot be used  with multiple processes
    --- missing some newly required fields

--- gunicorn/gunicorn_start.sh
+++ gunicorn/gunicorn_start.sh
@@ -8,4 +8,4 @@ if [ -n "${VIRTUAL_ENV}" ]; then
 fi
 
 echo "Starting new instances..."
-gunicorn --error-logfile run/log/deuce-errors.log --access-logfile run/log/deuce-access.log --log-level DEBUG -D deuce.transport.wsgi.app:app
+gunicorn -b 127.0.0.1:8081 -w 16 --error-logfile run/log/deuce-errors.log --access-logfile run/log/deuce-access.log --log-level DEBUG -D deuce.transport.wsgi.app:app
