8cae411ac428f1909de72f997936df8f823274a7
BenjamenMeyer <ben.meyer@rackspace.com>
2014-11-10 18:03:47 +0000

    Enhancement: Bastion
    - Added bastion wrapper
    - Added EOM example configuration, no details

--- apps/deuce_deploy_app.py
+++ apps/deuce_deploy_app.py
@@ -1,11 +1,15 @@
-from eom import rbac
 from eom import auth
+from eom import bastion
+from eom import rbac
 from deuce.transport.wsgi.app import app as deuce_app
 
 from oslo.config import cfg
 conf = cfg.CONF
 conf(project='eom', args=[])
+
 # Get the separated Redis Server for Auth
 auth_redis_client = auth.get_auth_redis_client()
+
 rbac_app = rbac.wrap(deuce_app)
-app = auth.wrap(rbac_app, auth_redis_client)
+auth_app = auth.wrap(rbac_app, auth_redis_client)
+app = bastion.wrap(deuce_app, auth_app)
